941771daef0be4d3db21be62e49242e85063b95a
mruthunjay <mruthunjaymuttu@gmail.com>
2024-10-15 19:46:54 +0000

    updating unsaved file

--- mongo-project/routes/userRoute.js
+++ mongo-project/routes/userRoute.js
@@ -52,7 +52,7 @@ routerU.get("/purchasedCourses", userMiddleware, async (req, res) => {
   const user = await User.findOne({
     username: req.headers.username,
   });
-  console.log(user.purchasedCourses);
+  // console.log(user.purchasedCourses);
   const courses = await Course.find({
     _id: {
       $in: user.purchasedCourses,
